fee6fce9ee5f94f9f8b6c9902c946c3c5468055d
TheodorDM <st129282@studem.spbu.ru>
2024-09-20 00:45:31 +0300

    my practice program

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "hello.hpp"
+
+using namespace std;
+
+int main()
+	{
+	hello():
+	cout << "Hello,";
+	string name;
+	while (name != "stop")
+		{
+		cout << name <<endl;
+	}
