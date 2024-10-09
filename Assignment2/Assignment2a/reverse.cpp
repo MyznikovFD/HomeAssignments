336b005e1276aabdd0f678c7bcb8ff4e3c9f9ab5
TheodorDM <st129282@studem.spbu.ru>
2024-10-09 20:42:18 +0300

    buffer for reading is made

--- Assignment2/Assignment2a/reverse.cpp
+++ Assignment2/Assignment2a/reverse.cpp
@@ -6,17 +6,23 @@ int main()
     {
     int size = std::filesystem::file_size("rabbit.txt");
     std::ifstream infile;
-    infile.open("rabbit.txt");
+    int *buffer = new int[size];
 
-
+    infile.open("rabbit.txt", std::ios::binary);
+    if (!infile.is_open())
+        std::cout << "Error infile is not opened!" << std::endl;
+    else
+        infile.read((char*)&buffer, size);
     infile.close();
-    std::cout << size << std::endl;
-}
 
-int rabbitsRecovery()
-    {
+    delete[] buffer;
+    
+
+
+
     std::ofstream inRabbit;
     inRabbit.open("rabbit.txt");
-    inRabbit << "White" << std::endl << "Rabbit";
-    inrabbit.close()
+    inRabbit << "White" << std::endl << "Rabbit" << std::endl;
+    inRabbit.close();
 }
+
