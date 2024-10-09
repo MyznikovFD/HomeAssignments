2f2a677f0056e8738a609fe3e0c83db18e28136f
TheodorDM <st129282@studem.spbu.ru>
2024-10-09 16:35:55 +0300

    opening and closing of file; counted filesize

--- Assignment2/Assignment2a/reverse.cpp
+++ Assignment2/Assignment2a/reverse.cpp
@@ -4,5 +4,19 @@
 
 int main()
     {
-    
+    int size = std::filesystem::file_size("rabbit.txt");
+    std::ifstream infile;
+    infile.open("rabbit.txt");
+
+
+    infile.close();
+    std::cout << size << std::endl;
+}
+
+int rabbitsRecovery()
+    {
+    std::ofstream inRabbit;
+    inRabbit.open("rabbit.txt");
+    inRabbit << "White" << std::endl << "Rabbit";
+    inrabbit.close()
 }
