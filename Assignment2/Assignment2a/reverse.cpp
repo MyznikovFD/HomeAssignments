2740ab473211ace0aa7e76000b2f3bbf44d39bda
TheodorDM <st129282@studem.spbu.ru>
2024-10-10 22:21:39 +0300

    algoryth is done

--- Assignment2/Assignment2a/reverse.cpp
+++ Assignment2/Assignment2a/reverse.cpp
@@ -5,26 +5,35 @@
 int main()
 {
     int size = std::filesystem::file_size("rabbit.txt");
-    std::ifstream infile;
     char *buffer = new char[size];
 
-    infile.open("rabbit.txt", std::ios::binary);
+    std::ifstream infile;
+    infile.open("rabbit.txt", std::ios::binary | std::ios::in);
     if (!infile.is_open())
         std::cout << "Error infile is not opened!" << std::endl;
     else
-        infile.read(buffer, size);
+        infile.read((char*)buffer, sizeof(buffer));
     infile.close();
+
     int bufElmnt;
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < (size / 2); i++)
     {
         bufElmnt = buffer[i];
-        buffer[i] = size;
-        
+        buffer[i] = buffer[size - i - 1];
+        buffer[size - i - 1] = bufElmnt;
     }
+
+    std::ofstream outfile;
+    outfile.open("reversedRabbit.bin", std::ios::binary | std::ios::out);
+    if (!outfile.is_open())
+        std::cout << "Error outfile is not opened!" << std::endl;
+    else
+        outfile.write((char*)buffer, sizeof(buffer));
+    outfile.close();
     delete[] buffer;
 
 
-
+    //contenting of rabbit.txt
     std::ofstream inRabbit;
     inRabbit.open("rabbit.txt");
     inRabbit << "White" << std::endl << "Rabbit" << std::endl;
