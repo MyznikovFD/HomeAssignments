716a66094a7bd8349f4da20247edb6be9792f6b6
TheodorDM <st129282@studem.spbu.ru>
2024-10-11 12:16:27 +0300

    problem with reading of input exists

--- Assignment2/Assignment2b/main.cpp
+++ Assignment2/Assignment2b/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
 
 int head = 0;
 const int stackSize = 50;
 double *stack = new double[stackSize];
 
 
-void push(std::string s)
+void strPush(std::string s)
 {
     double num = (double) atof(s.c_str());
     if (head > stackSize)
         std::cout << "ErrOr!! stack is overflowed!";
     else
         stack[head++] = num;
+}
+void doublePush(double num)
+{
+    if (head > stackSize)
+        std::cout << "ErrOr!! stack is overflowed!";
+    else
+        stack[head] = num;
 }
 
 double pop()
@@ -23,32 +29,32 @@ double pop()
 
 double opAddition()
 {
-return (pop()+stack[head]);
+return (stack[head] + pop());
 }
 double opDivision()
 {
-return (stack[head]/pop());
+return (stack[head] / pop());
 }
 double opDifference()
 {
-return (stack[head]-pop());
+return (stack[head] - pop());
 }
 double opMultiplication()
 {
-return (pop()*stack[head]);
+return (stack[head] * pop());
 }
 
 
 double operation(char cOpr)
 {
     if (cOpr == '+')
-         return (opAddition());
+        return (opAddition());
     if (cOpr == '/')
-         return (opDivision());
+        return (opDivision());
     if (cOpr == '-')
-         return (opDifference());
+        return (opDifference());
     if (cOpr == '*')
-         return (opMultiplication());
+        return(opMultiplication());
     else
         std::cout << "ErROr!! indefined operation!" << std::endl;
         exit(EXIT_FAILURE);
@@ -61,20 +67,24 @@ int main()
 {
     unsigned char c;
     std::string in;
-    int i = 0;
     unsigned char c0 = '0';
     unsigned char c9 = '9';
     int intC9 = (int) c9;
     int intC0 = (int) c0;
-    int intC = (int) c;
     while(std::cin >> in)
     {
-        c = in[i];
+        c = in[0];
+        int intC = (int) c;
         if (intC <= intC9; intC >= intC0)
-            push(in);
+           strPush(in);
         else
-            operation(c);
+           operation(c);
+        std::cout << '/';
+       int i = 0;
+       while (i <= head)
+            std::cout << stack[i] << std::endl;
+            i++;
     }
-    i++;
+    std::cout << stack[head];
     delete[] stack;
 }
