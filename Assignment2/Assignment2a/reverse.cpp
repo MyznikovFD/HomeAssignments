5e415e3d5cdd86801aedbdfbcb9357ad80cd2388
TheodorDM <st129282@studem.spbu.ru>
2024-12-15 18:29:05 +0300

    sourceFile is changed

--- Assignment2/Assignment2a/reverse.cpp
+++ Assignment2/Assignment2a/reverse.cpp
@@ -6,21 +6,17 @@
 
 int main()
 {
-    int size = std::filesystem::file_size("rabbit.txt");
+    int size = std::filesystem::file_size("source.txt");
     char *buffer = new char[size];
 
     std::ifstream infile;
-    infile.open("rabbit.txt", std::ios::binary | std::ios::in);
+    infile.open("source.txt", std::ios::binary | std::ios::in);
     if (!infile.is_open())
         std::cout << "Error infile is not opened!" << std::endl;
     else
         infile.read(buffer, size);
     infile.close();
 
-    for (int i = 0; i < (size); i++)
-    {
-        std::cout << buffer[i];
-    }
 
     int bufElmnt;
     for (int i = 0; i < (size / 2); i++)
@@ -31,9 +27,9 @@ int main()
     }
 
     std::ofstream outfile;
-    outfile.open("reversedRabbit.txt", std::ios::binary | std::ios::out);
+    outfile.open("result", std::ios::binary | std::ios::out);
     if (!outfile.is_open())
-        std::cout << "Error outfile is not opened!" << std::endl;
+        std::cout << "ErroR outfile is not opened!" << std::endl;
     else
         outfile.write((char*)buffer, size);
     outfile.close();
